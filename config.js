d0e5ca3d6adbe8ccf58cda008d4013ffc66ba22f
Piotr Prus <pprus94@gmail.com>
2022-12-20 15:05:20 +0100

    Adding files to project

---
+++ config.js
@@ -0,0 +1,10 @@
+export default {
+    port: 3001,
+    dbUrl: 'mongodb://localhost:27017/images',
+    allowedMimeTypes: [
+        'image/jpeg',
+        'image/png',
+        'image/jpg'
+    ]
+}
+
