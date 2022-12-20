79937761c4a96fa3ec36ceaf2279ba7b8b3e1d0a
Piotr Prus <pprus94@gmail.com>
2022-12-20 15:03:05 +0100

    Adding readme

--- config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    port: 3001,
-    dbUrl: 'mongodb://localhost:27017/images',
-    allowedMimeTypes: [
-        'image/jpeg',
-        'image/png',
-        'image/jpg'
-    ]
-}
